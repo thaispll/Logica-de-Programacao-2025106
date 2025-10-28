95a9ddf214dcc0d398c63ed9f69bcd0ae8754e02
Thais Leal <58890658+thaispll@users.noreply.github.com>
2025-10-27 21:34:57 -0300

    aula até dia 27.10

---
+++ Python/imposto.py
@@ -0,0 +1,8 @@
+#Crie um programa que inclua uma função chamada somaImposto.
+# Essa função deve receber dois parâmetros: taxaImposto, 
+# que representa a porcentagem do imposto sobre vendas, e custo,
+#  que é o valor do item antes da aplicação do imposto. 
+# A função deve calcular o valor do imposto a partir da taxa fornecida
+#  e adicionar esse valor ao custo inicial, retornando o novo custo 
+# já com o imposto incluído.
+
